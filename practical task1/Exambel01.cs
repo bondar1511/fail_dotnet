0799c4c38f3bdd5f216f35b31d860a0713300848
unknown <annaBondar>
2022-10-06 23:21:00 +0400

    написали программу

---
+++ practical task1/Exambel01.cs
@@ -0,0 +1,23 @@
+Console.WriteLine(start);
+
+int a = 2;
+int b = 3;
+int c = 7;
+int max = 0;
+
+if (a>b)
+{
+    int max = a;
+}
+else if (b>c)
+{
+    int max = b;
+
+}
+else if (c>a)
+{
+    int max = c;
+}
+
+Console.WriteLine(max);
+Console.WriteLine(end);
