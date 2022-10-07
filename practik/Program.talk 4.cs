ac9ddc9f439300b617c9afd89290959000bd24da
unknown <annaBondar>
2022-10-07 22:43:19 +0400

    Решение задачи 4, 1 пример

---
+++ practik/Program.talk 4.cs
@@ -0,0 +1,17 @@
+﻿int a = 2, b = 3, c = 7;
+
+if (a>b)
+{
+ Console.WriteLine ("Number Max = " +a);
+}
+ else if (b>c)
+{
+  Console.WriteLine ("Number Max = " +b);
+
+}
+else if (c>a)
+{
+Console.WriteLine ("Number Max = " +c);
+}
+
+Console.ReadLine();
