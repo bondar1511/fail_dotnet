94d69b2d7681eda2cc867d720378461736a53857
unknown <annaBondar>
2022-10-08 10:20:12 +0400

    Добавили решение задачи 6

---
+++ practik1/Program.talk6.cs
@@ -0,0 +1,21 @@
+﻿
+partial class Program
+{
+    static void Main(string[] args)
+    {
+
+        {
+            Console.WriteLine("Введите число : ");
+            int a = Convert.ToInt32(Console.Read());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("да, четное ");
+            }
+            else
+            {
+                Console.WriteLine("нет, нечетное ");
+            }
+
+        }
+    }
+}
