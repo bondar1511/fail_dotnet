c6ab5dba5a47cdc133b48c02d768c7e77a568878
unknown <annaBondar>
2022-10-08 23:25:00 +0400

    добавили перво числое из задачи

---
+++ homework/talk10/Program.talk10.cs
@@ -0,0 +1,12 @@
+﻿//Задача 10: Напишите метод, который принимает на вход трёхзначное число 
+// на выходе показывает вторую цифру этого числа.
+//456 -> 5, 782 -> 8, 918 -> 1
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = 456.ToString();
+        Console.WriteLine(str[1]);
+    }
+}
