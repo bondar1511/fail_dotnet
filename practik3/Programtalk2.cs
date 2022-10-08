57d91c24799fd975136280e8ff5ef4590be9d3fb
unknown <annaBondar>
2022-10-08 11:31:34 +0400

    Добавили решение задачи 2

---
+++ practik3/Programtalk2.cs
@@ -0,0 +1,13 @@
+﻿   
+partial class Program
+  {
+    static void Main(string[] args)
+    {
+        {  Console.WriteLine("Введите первое число:");
+        int a=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе число:");
+        int b=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(a>b?"Первое число больше второго":"Второе число больше первого");
+        }
+     }
+  }
