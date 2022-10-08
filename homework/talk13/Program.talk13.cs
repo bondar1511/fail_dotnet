d891b5311237c0a52faa4cccc82506ade706fb62
unknown <annaBondar>
2022-10-08 23:54:57 +0400

    добавили первое число из задачи

---
+++ homework/talk13/Program.talk13.cs
@@ -0,0 +1,13 @@
+﻿//Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 6, 78 -> третьей цифры нет, 32679 -> 6
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string str = 645.ToString();
+         Console.WriteLine(str[2]);
+    }
+}
+    
