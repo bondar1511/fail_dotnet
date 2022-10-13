61116f09ff7f0e8dda8efd144778b95e72e98873
unknown <annaBondar>
2022-10-13 23:34:09 +0400

    добавили решение задачи 19

---
+++ practik 08.10/talk 19/Programtalk19.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19/ Напишите программу, которая принимает на вход пятизначное число и проверяет,
+//является ли оно палиндромом. 14212 -> нет, 12821 -> да, 23432 -> да
+
+        int [] nums = {14212};
+        static void Palindrom(string x)
+        {  
+           char[] tx = x.ToCharArray();
+            Array.Reverse(tx);
+            string tx1 = new string(tx);
+            if (x == tx1)
+            {
+                Console.WriteLine("Палиндром");
+            }
+            else
+            {
+                Console.WriteLine("НЕТ");
+            }
+        }
