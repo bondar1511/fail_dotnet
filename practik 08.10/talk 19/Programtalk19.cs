20eb4b8efb8362c555f3c736d4ddbe625f04c2cc
unknown <annaBondar>
2022-10-14 23:40:22 +0400

    Добавили решение задачи 23

--- practik 08.10/talk 19/Programtalk19.cs
+++ practik 08.10/talk 19/Programtalk19.cs
@@ -1,8 +1,9 @@
 ﻿// Задача 19/ Напишите программу, которая принимает на вход пятизначное число и проверяет,
 //является ли оно палиндромом. 14212 -> нет, 12821 -> да, 23432 -> да
 
-        int [] nums = {14212};
-        static void Palindrom(string x)
+      int [] nums = {14212};
+        
+        static void  Palindrom(string x)
         {  
            char[] tx = x.ToCharArray();
             Array.Reverse(tx);
@@ -16,3 +17,5 @@
                 Console.WriteLine("НЕТ");
             }
         }
+
+
---
+++ practik 08.10/talk 23/Programtalk23.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27;5 -> 1, 8, 27, 64, 125
+
+
+internal class Program
+{
+    private const int V = 100;
+
+    private static void Main(string[] args)
+    {
+        int i = 0;
+        int result = 0;
+
+        for (i = 1; i < V; i++)
+        {
+            result = i * i * i;
+            Console.Write(result);
+       
+        }
+
+    }
+}
