95a27e5a0027b41671e886f442b0d87962d2a767
unknown <annaBondar>
2022-10-15 23:10:11 +0400

    Добавили решение задачи 29, первая строка

---
+++ homework/practik 09.10/talk29/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите метод, который задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+          int[] arr = new int[] { 1, 2, 5, 7, 19 };
+      
+         Console.WriteLine(string.Join(' ', arr));
+    }
+}
+
