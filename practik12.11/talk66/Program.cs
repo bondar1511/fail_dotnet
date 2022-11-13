3bf4b6c63071a628e860f949cfc821dbb94a234b
unknown <annaBondar>
2022-11-13 23:11:57 +0400

    добавили решение задачи

---
+++ practik12.11/talk66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            Console.WriteLine("Введите значение M: ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите значение N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            
+                    
+            while (M <= N)
+            {
+               sum = M + 1;
+                sum += 1;
+                return;                          
+            }         
+                Console.WriteLine($" Сумма натуральных чисел: {sum}");  
+           }
+
+        
+                 
+       
+        }
+    }
---
+++ practik12.11/talk68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+           Console.WriteLine("Введите значение m: ");
+            Console.WriteLine("Введите значение n: ");
+           static uint A(uint n, uint m);
+           
+            if (m == 0)
+            {
+                return n + 1;
+            }
+            else if (n == 0)
+            {
+                return A(m - 1, 1);
+            }
+            else
+            {
+                return A(m - 1, A(m, n - 1));
+            }
+
+      Console.WriteLine(A(m, n)); 
+ 
